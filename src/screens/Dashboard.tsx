f49cc1f5c1650f0028acfa44a60f7530accc0dc8
mehmet <mehmet.izci@mendix.com>
2021-04-04 14:40:50 +0200

    Add logout in dashboard

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,6 +1,16 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useContext } from "react";
+import { Button, ScrollView } from "react-native";
+import { observer } from "mobx-react-lite";
+import { RootStoreContext } from "../stores/RootStore";
 
-export const Dashboard = () => {
-  return <Text>Dashboard</Text>;
-};
+export const Dashboard = observer(() => {
+  const rootStore = useContext(RootStoreContext);
+  return (
+    <ScrollView>
+      <Button
+        title={"Logout"}
+        onPress={() => rootStore.profileStore.logout()}
+      />
+    </ScrollView>
+  );
+});
