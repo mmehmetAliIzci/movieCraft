4e5422550b5e456127f43054e6a61d0a69c7835e
mehmet <mehmet.izci@mendix.com>
2021-04-05 18:07:45 +0200

    Revisit login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,16 @@
 import React, { useContext, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { Button, ScrollView, StyleSheet, TextInput } from "react-native";
+import {
+  Dimensions,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
 import { RootStoreContext } from "../stores/RootStore";
+import { Pressable } from "../components/Pressable";
+import { borderRadius, colors } from "../theme/styleVariables";
 
 export const Login = observer(() => {
   const rootStore = useContext(RootStoreContext);
@@ -12,41 +21,54 @@ export const Login = observer(() => {
 
   return (
     <ScrollView style={{ flex: 1 }} keyboardShouldPersistTaps={"handled"}>
-      <TextInput
-        style={style.input}
-        autoCapitalize={"none"}
-        placeholder={"Here's Johnny !"}
-        onChangeText={text => {
-          setCredentials({ ...credentials, username: text });
-        }}
-      />
-      <TextInput
-        style={style.input}
-        autoCapitalize={"none"}
-        secureTextEntry={true}
-        placeholder={"********"}
-        onChangeText={text =>
-          setCredentials({ ...credentials, password: text })
-        }
-      />
-      <Button
-        onPress={async () => {
-          await rootStore.profileStore.login(
-            credentials.username,
-            credentials.password,
-          );
-        }}
-        title={"Login"}
-      />
+      <ImageBackground
+        resizeMode={"cover"}
+        source={require("../assets/loginBackground.jpg")}
+        style={style.imageBackground}>
+        <TextInput
+          style={style.input}
+          autoCapitalize={"none"}
+          placeholder={"Username"}
+          onChangeText={text => {
+            setCredentials({ ...credentials, username: text });
+          }}
+        />
+        <TextInput
+          style={style.input}
+          autoCapitalize={"none"}
+          secureTextEntry={true}
+          placeholder={"********"}
+          onChangeText={text =>
+            setCredentials({ ...credentials, password: text })
+          }
+        />
+        <Pressable
+          onPress={async () => {
+            await rootStore.profileStore.login(
+              credentials.username,
+              credentials.password,
+            );
+          }}>
+          <Text style={{ color: colors.white }}>Login</Text>
+        </Pressable>
+      </ImageBackground>
     </ScrollView>
   );
 });
 
 const style = StyleSheet.create({
+  imageBackground: {
+    height: Dimensions.get("window").height,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+  },
   input: {
-    height: 50,
-    borderWidth: 4,
-    borderColor: "gray",
-    borderRadius: 5,
+    height: 30,
+    borderWidth: StyleSheet.hairlineWidth,
+    padding: 5,
+    margin: 10,
+    borderColor: "black",
+    borderRadius,
+    color: "black",
   },
 });
